6dd698bd0df5ed16a4c8e611307a95ad7f1cf9f3
MiguelF999 <amiguel.faria06@gmail.com>
2026-01-30 20:41:02 +0000

    metal stairs, metal hammer, shelf, water pipe

---
+++ Assets/Scripts/Shelf.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+
+    public float var = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Action()
+    {
+        
+    }
+}
