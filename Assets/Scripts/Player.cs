da418812cd123968392a963b903d653ad5d92442
TARB2006 <tiagoarbotelho@gmail.com>
2026-01-31 09:57:08 +0000

    possessing mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     private float input_force = 15.0f;
     private float friction = 8.0f;
 
+    private bool not_again = false;
+
+    private GameObject possessed_object;
+
     private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,11 +24,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+
+        if (Input.GetKeyUp("space"))
+        {
+            not_again = false;
+        }
     }
 
     private void Move()
     {
-        cur_speed = rb.linearVelocity;
 
         float horizontal_input = Input.GetAxisRaw("Horizontal");
         float vertical_input = Input.GetAxisRaw("Vertical");
@@ -49,19 +57,46 @@ public class Player : MonoBehaviour
         if (cur_speed.y > max_speed) cur_speed.y = max_speed;
         if (cur_speed.y < -max_speed) cur_speed.y = -max_speed;
 
+        if(possessed_object != null)
+        {
+            Rigidbody2D possessed_object_rb = possessed_object.GetComponent<Rigidbody2D>();
+            possessed_object_rb.linearVelocity = cur_speed;
+            rb.position = possessed_object_rb.position;
+        }
+        else
+        {
+            rb.linearVelocity = cur_speed;
+        }
+            
 
-        rb.linearVelocity = cur_speed;
     }
 
-    private float Min(float a, float b)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (a < b) return a;
-        return b;
-    }
 
-    private float Max(float a, float b)
-    {
-        if (a > b) return a;
-        return b;
+        if (collision.CompareTag("possessable") && Input.GetKey("space") && ! not_again)
+        {
+            not_again = true;
+
+            if (possessed_object == null)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                possessed_object = collision.gameObject;
+                rb.transform.position = new Vector3(possessed_object.transform.position.x , possessed_object.transform.position.y, 1);
+            }
+            else
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                rb.transform.position = new Vector3(possessed_object.transform.position.x, possessed_object.transform.position.y - 1, -1);
+                Rigidbody2D possessed_object_rb = possessed_object.GetComponent<Rigidbody2D>();
+                possessed_object_rb.linearVelocity = new Vector2(0, 0);
+                possessed_object_rb.angularVelocity = 0;
+                possessed_object = null;
+            }
+
+            cur_speed = new Vector2(0, 0);
+        }
     }
 }
