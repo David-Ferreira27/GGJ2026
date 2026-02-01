61f794784a10296ed3c89faba2938d1e732bcbe7
MiguelF999 <amiguel.faria06@gmail.com>
2026-02-01 11:32:39 +0000

    upgraded monster movement

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
@@ -13,6 +14,8 @@ public class Monster : MonoBehaviour
 	public bool aggro = false;
 
 	private Vector3 lastSeen;
+	private float rotateTimer = -1f;
+	private Vector3 lastPathPos;
 
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -47,7 +50,7 @@ public class Monster : MonoBehaviour
 				float rayrad = (angl + rayangl * (i - nrays / 2f)) * Mathf.Deg2Rad;
 				Vector2 d = new Vector2((float)Mathf.Cos(rayrad), (float)Mathf.Sin(rayrad));
 				d.Normalize();
-				ray = Physics2D.Raycast(gameObject.transform.position + new Vector3((float)Mathf.Cos(rad) * offset, (float)Mathf.Sin(rad), 0f) * offset, d, 999f);
+				ray = Physics2D.Raycast(gameObject.transform.position + new Vector3((float)Mathf.Cos(rad) * offset, (float)Mathf.Sin(rad) * offset, 0f), d, 999f);
 				if (ray.collider != null && ray.collider.gameObject.tag == "Player")
 				{
 					newaggro = true;
@@ -71,32 +74,64 @@ public class Monster : MonoBehaviour
 
 		if (aggro)
 		{
+			rotateTimer = 5f;
 			lastSeen = player.transform.position;
 		}
 	}
 
 	private void Rotate()
 	{
+		if(rb.linearVelocity == new Vector2(0, 0) && rotateTimer > 0f)
+		{
+			angl++;
+			rotateTimer -= Time.deltaTime;
+		}
+
+		if(rotateTimer < 0f && InPath())
+		{
+			if(angl!=180f && angl!=0f){angl = 0f;}
+			if(gameObject.transform.position.x>5f){angl=180f;}
+			if(-5f>gameObject.transform.position.x){angl = 0f;}
+			lastPathPos = gameObject.transform.position;
+		}
+		if(rotateTimer < 0f && !InPath())
+		{
+			Vector2 d = lastPathPos - transform.position;
+			d.Normalize();
+			angl = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
+		}
 
 		if (aggro)
 		{
 			Vector2 d = lastSeen - transform.position;
 			d.Normalize();
 			angl = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
-
-
 		}
 
 		transform.rotation = Quaternion.Euler(0f, 0f, angl-90f);
+
 		//angl -= 0.1f;
 	}
 
 	private void Move()
 	{
-		Vector2 speed = new Vector2(Mathf.Cos(rad) * max_speed, Mathf.Sin(rad) * max_speed);
+		Vector2 speed = new Vector2(0,0);
+		if(!aggro && (rotateTimer < 0f || Math.Abs(gameObject.transform.position.x - lastSeen.x) > max_speed || Math.Abs(gameObject.transform.position.y - lastSeen.y) > max_speed) || aggro){speed = new Vector2(Mathf.Cos(rad) * max_speed, Mathf.Sin(rad) * max_speed);}
 
 		rb.linearVelocity = speed;
 
+	}
+
+	private bool InPath()
+	{
+		if(Near(gameObject.transform.position.y,0f,max_speed) && -5f-max_speed<gameObject.transform.position.x && gameObject.transform.position.x<5f+max_speed){return true;}
+		return false;
+	}
+
+	private bool Near(float myPos, float targetPos, float offset)
+	{
+		if(Math.Abs(myPos-targetPos) < offset){return true;}
+		return false;
 	}
 
 	
